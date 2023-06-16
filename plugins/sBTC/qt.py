84b1968777b7bc29743bfbb38d3e1bacbc431f06
Pato Gomez <pato2k5@gmail.com>
2023-06-15 23:26:01 -0600

    Able to send BTC from within sBTC plugin, fetching sBTC wallet address, adding address and displaying STX and sBTC balance

--- plugins/sBTC/qt.py
+++ plugins/sBTC/qt.py
@@ -1,34 +1,46 @@
-from PyQt5.QtCore import Qt, QThread, pyqtSignal
-from PyQt5.QtWidgets import QVBoxLayout, QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem, QHeaderView, QMessageBox, QWidget, QTabWidget, QInputDialog
-from electrum.plugin import BasePlugin, hook
-from electrum.i18n import _
 import requests
+import os
+from electrum.plugin import BasePlugin, hook
+from electrum.bitcoin import is_address
+from electrum.i18n import _
+from electrum.gui.qt.util import WindowModalDialog, EnterButton
+from electrum.gui.qt.main_window import ElectrumWindow
+from electrum.wallet import InternalAddressCorruption
+from electrum.transaction import PartialTxOutput
+from PyQt5.QtWidgets import QVBoxLayout, QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem, QHeaderView, QMessageBox, QWidget, QTabWidget, QInputDialog, QComboBox
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+
 
 class BalanceFetcher(QThread):
-    balanceFetched = pyqtSignal(str, float)
+    balancesFetched = pyqtSignal(str, float, float)
 
     def __init__(self, address):
         super().__init__()
         self.address = address
 
     def run(self):
-        balance = self.get_balance(self.address)
-        self.balanceFetched.emit(self.address, balance)
+        balance_stx, balance_sBTC = self.get_balance(self.address)
+        self.balancesFetched.emit(self.address, balance_stx, balance_sBTC)
+
 
     def get_balance(self, address):
         print(address)
         try:
-            response = requests.get(f'https://testnet.stx.eco/bridge-api/testnet/v1/sbtc/address/{address}/balance')
-            print(response)
-            print(response.text)  # Print the response content
+            url = "https://testnet.stx.eco/bridge-api/testnet/v1/sbtc/address/balances"
+            payload = {
+                "stxAddress": address
+            }
+            headers = {
+                "Content-Type": "application/json"
+            }
+            response = requests.post(url, json=payload, headers=headers)
             data = response.json()
-            print(data)
-            balance = data['balance']
-            return balance
+            balance_stx = float(data["stacksTokenInfo"]["stx"]["balance"])
+            balance_sBTC = float(data["sBTCBalance"])
+            return balance_stx, balance_sBTC
         except Exception as e:
             print(f"Failed to fetch balance for address {address}: {e}")
-            return 0
-
+            return 0, 0
 
 
 class Plugin(BasePlugin):
@@ -71,38 +83,71 @@ class SBTC_Tab(QWidget):
         self.setLayout(layout)
 
     def create_deposit_tab(self, window):
-        widget = QWidget()
-        vbox = QVBoxLayout(widget)
+        # This function creates the interface for the new "Send BTC" tab.
+        widget = WindowModalDialog(window, _('Send BTC'))  # Create a new dialog window
+        widget.setWindowFlags(widget.windowFlags() & ~Qt.Dialog)  # Set the window flags
+        vbox = QVBoxLayout(widget)  # Create a new vertical box layout
 
-        vbox.addWidget(QLabel(_("BTC Amount to Deposit:")))
-        self.amount_input = QLineEdit()
+        # Create the interface elements for the send tab
+        vbox.addWidget(QLabel(_("Amount (BTC):")))  
+        self.amount_input = QLineEdit() 
         vbox.addWidget(self.amount_input)
 
         vbox.addWidget(QLabel(_("Associated STX Address:")))
         self.stx_address_input = QLineEdit()
         vbox.addWidget(self.stx_address_input)
 
+        vbox.addWidget(QLabel(_("My BTC wallet:")))
+        self.btc_wallet_address_label = QLabel()
+        vbox.addWidget(self.btc_wallet_address_label)
+
         vbox.addWidget(QLabel(_("sBTC wallet:")))
         self.wallet_address_label = QLabel()
         vbox.addWidget(self.wallet_address_label)
 
-        vbox.addWidget(QLabel(_("Transaction Fee:")))
-        self.fee_label = QLabel()
-        vbox.addWidget(self.fee_label)
+        vbox.addWidget(QLabel(_("Recipient Address:")))
+        self.address_input = QLineEdit()
+        vbox.addWidget(self.address_input)
 
-        send_button = QPushButton(_("Deposit BTC for sBTC"))
-        send_button.clicked.connect(self.deposit_btc)
+        vbox.addWidget(QLabel(_("Transaction Fee (BTC):")))
+        self.fee_label = QLabel()  # Create the fee label
+        vbox.addWidget(self.fee_label)  # Add the fee label to the layout
+
+        # Create a combo box for fee selection
+        self.fee_combo = QComboBox()
+        vbox.addWidget(self.fee_combo)
+
+        send_button = EnterButton(_('Deposit BTC'), lambda: self.prompt_password(window))  # Create the send button
         vbox.addWidget(send_button)
 
-        self.tx_status_label = QLabel()
-        vbox.addWidget(self.tx_status_label)
+        # Fetch fee estimates and populate the combo box
+        fee_estimates = self.fetch_fee_estimates()
+        if fee_estimates:
+            fee_options = [
+                f"Low: {fee_estimates['low_fee_per_kb'] / 1e8} BTC",
+                f"Medium: {fee_estimates['medium_fee_per_kb'] / 1e8} BTC",
+                f"High: {fee_estimates['high_fee_per_kb'] / 1e8} BTC"
+            ]
+            self.fee_combo.addItems(fee_options)
 
         # Fetch and display the sBTC wallet address
         wallet_address = self.fetch_sbtc_wallet_address()
         self.wallet_address_label.setText(wallet_address)
 
+        btc_wallet_address = self.fetch_btc_wallet_address(window)
+        self.btc_wallet_address_label.setText(btc_wallet_address)
+
         widget.setLayout(vbox)
         return widget
+
+    def fetch_fee_estimates(self):
+        try:
+            response = requests.get('https://testnet.stx.eco/bridge-api/testnet/v1/btc/blocks/fee-estimate')
+            data = response.json()
+            return data['feeInfo']
+        except Exception as e:
+            print(f"Failed to fetch fee estimates: {e}")
+            return None
 
     def fetch_sbtc_wallet_address(self):
         try:
@@ -114,9 +159,74 @@ class SBTC_Tab(QWidget):
             print(f"Failed to fetch sBTC wallet address: {e}")
             return "N/A"
 
+    def fetch_btc_wallet_address(self, window):
+        try:
+            wallet = window.wallet  # Get the wallet object
+            address = wallet.get_addresses()[0]  # Get the first address from the wallet
+            return address
+        except Exception as e:
+            print(f"Failed to fetch BTC wallet address: {e}")
+            return "N/A"
 
-    def deposit_btc(self):
-        pass
+    def prompt_password(self, window):
+        # This function is called when the send button is pressed. It prompts the user for their password.
+        password, ok = QInputDialog.getText(None, _('Enter Password'), _('Please enter your password'), QLineEdit.Password)
+        if ok:
+            self.send_btc(window, password)
+
+    def send_btc(self, window, password):
+        # This function sends the bitcoin transaction.
+        try:
+            amount_btc = float(self.amount_input.text())  # Get the amount of bitcoin to send
+            recipient_address = self.address_input.text()  # Get the recipient's address
+            fee_option = self.fee_combo.currentText()
+
+            # Extract the fee amount from the selected option
+            fee_btc = float(fee_option.split(":")[1].strip().split(" ")[0])
+
+            # Validate the BTC amount input
+            if not amount_btc:
+                raise ValueError("BTC amount is required")
+            try:
+                amount_btc = float(amount_btc)
+            except ValueError:
+                raise ValueError("Invalid BTC amount")
+
+            # Check if the recipient address is valid
+            if not is_address(recipient_address):
+                raise ValueError("Invalid recipient address")
+
+            amount_sat = int(amount_btc * 1e8)  # Convert the amount to satoshis
+            wallet = window.wallet  # Get the wallet object
+
+            fee = int(fee_btc * 1e8)  # Convert the fee to satoshis
+
+            self.fee_label.setText(f"{fee_btc:.8f}")  # Display the selected fee
+
+            output = PartialTxOutput.from_address_and_value(recipient_address, amount_sat)  # Create the transaction output
+            outputs = [output]
+
+            coins = wallet.get_spendable_coins(None)  # Get the coins that can be spent
+            tx = wallet.make_unsigned_transaction(coins=coins, outputs=outputs, fee=fee, rbf=True)  # Create the unsigned transaction
+            wallet.sign_transaction(tx, str(password))  # Sign the transaction
+            tx_hash = window.network.run_from_another_thread(window.network.broadcast_transaction(tx))  # Broadcast the transaction
+
+            if tx_hash:
+                wallet.add_transaction(tx_hash, tx)  # Add the transaction to the wallet
+                wallet.save_db()  # Save the wallet
+
+            # Display a message box confirming the transaction
+            QMessageBox.information(None, _('Transaction Success'), _('Transaction Success! Please check History to view your transaction, Stacks and sBTC Rules!!'))
+        except ValueError as e:
+            # If there was an error, display a message box with the error
+            QMessageBox.critical(None, _('Transaction Error'), _('Error sending BTC: {}').format(e))
+        except InternalAddressCorruption as e:
+            # If there was an error, display a message box with the error
+            QMessageBox.critical(None, _('Transaction Error'), _('Error sending BTC: {}').format(e))
+        except Exception as e:
+            # If there was an error, display a message box with the error
+            QMessageBox.critical(None, _('Transaction Error'), _('Error sending BTC: {}').format(e))
+
 
     def create_reclaim_deposit_tab(self, window):
         widget = QWidget()
@@ -153,8 +263,8 @@ class SBTC_Tab(QWidget):
         vbox = QVBoxLayout(widget)
 
         vbox.addWidget(QLabel(_("BTC Amount to Withdraw:")))
-        self.amount_input = QLineEdit()
-        vbox.addWidget(self.amount_input)
+        self.amount_withdraw_input = QLineEdit()
+        vbox.addWidget(self.amount_withdraw_input)
 
         vbox.addWidget(QLabel(_("STX Address:")))
         self.stx_address_input = QLineEdit()
@@ -215,8 +325,8 @@ class SBTC_Tab(QWidget):
         vbox.addWidget(add_button)
 
         self.summary_table = QTableWidget()
-        self.summary_table.setColumnCount(2)
-        self.summary_table.setHorizontalHeaderLabels(["STX Address", "sBTC Balance"])
+        self.summary_table.setColumnCount(3)
+        self.summary_table.setHorizontalHeaderLabels(["STX Address", "STX Balance", "sBTC Balance"])
         vbox.addWidget(self.summary_table)
 
         remove_button = QPushButton(_("Remove Selected Address"))
@@ -232,25 +342,27 @@ class SBTC_Tab(QWidget):
         self.summary_table.setItem(self.summary_table.rowCount() - 1, 0, QTableWidgetItem(address))
 
         fetcher = BalanceFetcher(address)
-        fetcher.balanceFetched.connect(self.update_balance)
+        fetcher.balancesFetched.connect(self.update_balance)
         fetcher.finished.connect(lambda: self.cleanup(fetcher))
         fetcher.start()
         self.fetchers.append(fetcher)
 
         if address in self.balance_btc:  # Display the balance if already fetched
-            balance = self.balance_btc[address]
-            self.summary_table.setItem(self.summary_table.rowCount() - 1, 1, QTableWidgetItem(str(balance)))
+            stx_balance, sbtc_balance = self.balance_btc[address]
+            self.summary_table.setItem(self.summary_table.rowCount() - 1, 1, QTableWidgetItem(str(stx_balance)))
+            self.summary_table.setItem(self.summary_table.rowCount() - 1, 2, QTableWidgetItem(str(sbtc_balance)))
 
-
-    def update_balance(self, address, balance):
-        self.balance_btc[address] = float(balance)  # Update the balance_btc dictionary
+    def update_balance(self, address, stx_balance, sbtc_balance):
+        self.balance_btc[address] = (stx_balance, sbtc_balance)
 
         for i in range(self.summary_table.rowCount()):
             if self.summary_table.item(i, 0).text() == address:
-                btc_balance_item = QTableWidgetItem(str(balance))
-                self.summary_table.setItem(i, 1, btc_balance_item)  # Set the QTableWidgetItem in the "BTC Balance" column
+                stx_balance = stx_balance / 1000000
+                stx_balance_item = QTableWidgetItem(str(stx_balance))
+                sbtc_balance_item = QTableWidgetItem(str(sbtc_balance))
+                self.summary_table.setItem(i, 1, stx_balance_item)
+                self.summary_table.setItem(i, 2, sbtc_balance_item)
                 break
-
 
     def remove_address(self):
         current_row = self.summary_table.currentRow()
